2e01319674c9074ead212ba59388c2324e82f0cd
Mike <mdoyle13@gmail.com>
2013-02-24 15:55:08 -0600

    getting deployment setup

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,38 @@
+require "rvm/capistrano"
+require "bundler/capistrano"
+
+set :rvm_type, :system
 set :application, "mikedoyleweb.com"
-set :repository,  "set your repository location here"
+set :repository,  "https://github.com/mdoyle13/mikedoyleweb3.2.git"
+set :deploy_to, "/var/www/mikedoyleweb.com"
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :deploy_via, :remote_cache
+set :user, "root"
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, "50.56.208.105"                          # Your HTTP server, Apache/etc
+role :app, "50.56.208.105"                          # This may be the same as your `Web` server
+role :db,  "50.56.208.105", :primary => true # This is where Rails migrations will run
 
 # if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+after "deploy:restart", "deploy:cleanup"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+#If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+  desc "Symlink shared configs and folders on each release."
+   task :symlink_shared do
+     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+     run "ln -nfs #{shared_path}/uploads  #{release_path}/public/uploads"
+   end
+end
+
+after 'deploy:update_code', 'deploy:symlink_shared'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Mikedoyleweb32::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -49,7 +49,7 @@ Mikedoyleweb32::Application.configure do
   # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
 
   # Enable threaded mode
   # config.threadsafe!
