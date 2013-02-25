ca7a0ba10a8b752f0ab157ca7b72ec4a8f35dcbd
Mike <mdoyle13@gmail.com>
2013-02-24 19:03:23 -0600

    fix for work not displaying all edit fields

--- app/admin/works.rb
+++ app/admin/works.rb
@@ -1,8 +1,8 @@
 ActiveAdmin.register Work do
   
   controller do
-    @work = Work.new
-    @new_attachment = @work.attachments.build
+    #@work = Work.new
+    #@new_attachment = @work.attachments.build
   end
   
   form do |f|
@@ -11,7 +11,7 @@ ActiveAdmin.register Work do
       f.input :project_url
       f.input :description
       f.input :tag_list
-      f.input :featured_thumbnail, :as => :file, :hint => image_tag(f.object.featured_thumbnail, :width => 200) unless f.object.featured_thumbnail.blank?
+      f.input :featured_thumbnail, :as => :file, :hint => ( image_tag(f.object.featured_thumbnail, :width => 200) unless f.object.featured_thumbnail.blank?)
       f.has_many :attachments do |p|
        
         p.input :file, :hint => ( p.template.image_tag(p.object.file_url, :width => "200")  + link_to("Delete Attachment", admin_attachment_path(p.object), :method => :delete, :confirm => "Are you sure") if p.object.file_url )
