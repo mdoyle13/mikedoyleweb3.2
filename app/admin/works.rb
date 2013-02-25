119220975518ed426f4f76f55982817df8949d09
Mike <mdoyle13@gmail.com>
2013-02-24 19:22:39 -0600

    admin fixes

--- app/admin/works.rb
+++ app/admin/works.rb
@@ -13,7 +13,6 @@ ActiveAdmin.register Work do
       f.input :tag_list
       f.input :featured_thumbnail, :as => :file, :hint => ( image_tag(f.object.featured_thumbnail, :width => 200) unless f.object.featured_thumbnail.blank?)
       f.has_many :attachments do |p|
-       
         p.input :file, :hint => ( p.template.image_tag(p.object.file_url, :width => "200")  + link_to("Delete Attachment", admin_attachment_path(p.object), :method => :delete, :confirm => "Are you sure") if p.object.file_url )
       end
     end
